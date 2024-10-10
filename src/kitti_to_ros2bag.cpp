af70a70283c87dda00e964c1b162937943e5509c
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-10-10 19:30:24 +0900

    Fix cv_bridge header file for jazzy

--- src/kitti_to_ros2bag.cpp
+++ src/kitti_to_ros2bag.cpp
@@ -15,7 +15,7 @@
 // ROS header
 #include <tf2/utils.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 
 // local header
