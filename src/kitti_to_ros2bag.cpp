ff39dcf0a763265a214ccfee0fa5415935451c8a
Yi-Chen Zhang <chris7462@gmail.com>
2025-10-02 14:42:43 -0400

    Bump to Jazzy (#8)
    * switch to mcap
    
    * working version
    
    * fix coding style
    
    * final version

--- src/kitti_to_ros2bag.cpp
+++ src/kitti_to_ros2bag.cpp
@@ -17,6 +17,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 #include <pcl_conversions/pcl_conversions.h>
+#include <rosbag2_storage/storage_options.hpp>
 
 // local header
 #include "kitti_to_ros2bag/kitti_to_ros2bag.hpp"
@@ -37,9 +38,53 @@ Kitti2BagNode::Kitti2BagNode()
   get_all_timestamps();
 
   writer_ = std::make_unique<rosbag2_cpp::Writer>();
-  writer_->open(output_bag_name);
+
+  // Configure storage options for MCAP format
+  rosbag2_storage::StorageOptions storage_options;
+  storage_options.uri = output_bag_name;
+  storage_options.storage_id = "mcap";
+
+  // Configure converter options
+  rosbag2_cpp::ConverterOptions converter_options;
+  converter_options.input_serialization_format = "cdr";
+  converter_options.output_serialization_format = "cdr";
+
+  writer_->open(storage_options, converter_options);
+
+  // Create topics with metadata
+  create_topics();
 
   timer_ = create_wall_timer(100ms, std::bind(&Kitti2BagNode::on_timer_callback, this));
+}
+
+void Kitti2BagNode::create_topics()
+{
+  // Camera image topics
+  create_topic("/kitti/camera/gray/left/image_raw", "sensor_msgs/msg/Image");
+  create_topic("/kitti/camera/gray/left/camera_info", "sensor_msgs/msg/CameraInfo");
+  create_topic("/kitti/camera/gray/right/image_raw", "sensor_msgs/msg/Image");
+  create_topic("/kitti/camera/gray/right/camera_info", "sensor_msgs/msg/CameraInfo");
+  create_topic("/kitti/camera/color/left/image_raw", "sensor_msgs/msg/Image");
+  create_topic("/kitti/camera/color/left/camera_info", "sensor_msgs/msg/CameraInfo");
+  create_topic("/kitti/camera/color/right/image_raw", "sensor_msgs/msg/Image");
+  create_topic("/kitti/camera/color/right/camera_info", "sensor_msgs/msg/CameraInfo");
+
+  // OXTS topics
+  create_topic("/kitti/oxts/gps/fix", "sensor_msgs/msg/NavSatFix");
+  create_topic("/kitti/oxts/gps/vel", "geometry_msgs/msg/TwistStamped");
+  create_topic("/kitti/oxts/imu", "sensor_msgs/msg/Imu");
+
+  // Velodyne topic
+  create_topic("/kitti/velo", "sensor_msgs/msg/PointCloud2");
+}
+
+void Kitti2BagNode::create_topic(const std::string & topic_name, const std::string & topic_type)
+{
+  rosbag2_storage::TopicMetadata topic_metadata;
+  topic_metadata.name = topic_name;
+  topic_metadata.type = topic_type;
+  topic_metadata.serialization_format = "cdr";
+  writer_->create_topic(topic_metadata);
 }
 
 void Kitti2BagNode::on_timer_callback()
@@ -54,57 +99,57 @@ void Kitti2BagNode::on_timer_callback()
 
     if (dir == "image_00") {
       auto msg = convert_image_to_msg(filename, timestamp, "mono8", "cam0_link");
-      writer_->write(msg, "kitti/camera/gray/left/image_raw", timestamp);
+      write_message(msg, "/kitti/camera/gray/left/image_raw", timestamp);
       if (calib_flag) {
         auto msg = convert_calib_to_msg(calib_file, timestamp, "0", "cam0_link");
         // Have to add some offset to the timestamp, otherwise it will overwrite the previous message
         rclcpp::Time tmp = rclcpp::Time(timestamp.nanoseconds() + 10);
-        writer_->write(msg, "kitti/camera/gray/left/camera_info", tmp);
+        write_message(msg, "/kitti/camera/gray/left/camera_info", tmp);
       }
     } else if (dir == "image_01") {
       auto msg = convert_image_to_msg(filename, timestamp, "mono8", "cam1_link");
-      writer_->write(msg, "kitti/camera/gray/right/image_raw", timestamp);
+      write_message(msg, "/kitti/camera/gray/right/image_raw", timestamp);
       if (calib_flag) {
         auto msg = convert_calib_to_msg(calib_file, timestamp, "1", "cam1_link");
         rclcpp::Time tmp = rclcpp::Time(timestamp.nanoseconds() + 10);
-        writer_->write(msg, "kitti/camera/gray/right/camera_info", tmp);
+        write_message(msg, "/kitti/camera/gray/right/camera_info", tmp);
       }
     } else if (dir == "image_02") {
       auto msg = convert_image_to_msg(filename, timestamp, "bgr8", "cam2_link");
-      writer_->write(msg, "kitti/camera/color/left/image_raw", timestamp);
+      write_message(msg, "/kitti/camera/color/left/image_raw", timestamp);
       if (calib_flag) {
         auto msg = convert_calib_to_msg(calib_file, timestamp, "2", "cam2_link");
         rclcpp::Time tmp = rclcpp::Time(timestamp.nanoseconds() + 10);
-        writer_->write(msg, "kitti/camera/color/left/camera_info", tmp);
+        write_message(msg, "/kitti/camera/color/left/camera_info", tmp);
       }
     } else if (dir == "image_03") {
       auto msg = convert_image_to_msg(filename, timestamp, "bgr8", "cam3_link");
-      writer_->write(msg, "kitti/camera/color/right/image_raw", timestamp);
+      write_message(msg, "/kitti/camera/color/right/image_raw", timestamp);
       if (calib_flag) {
         auto msg = convert_calib_to_msg(calib_file, timestamp, "3", "cam3_link");
         rclcpp::Time tmp = rclcpp::Time(timestamp.nanoseconds() + 10);
-        writer_->write(msg, "kitti/camera/color/right/camera_info", tmp);
+        write_message(msg, "/kitti/camera/color/right/camera_info", tmp);
       }
     } else if (dir == "oxts") {
       // parse the oxts data
-      std::vector<std::string> oxts_parsed_array = parse_file_data(filename, " ");
+      std::vector<std::string> oxts_parsed_array = parse_file_data(filename);
 
       // write the GPS data to bag
       auto gps_msg = convert_oxts_to_gps_msg(oxts_parsed_array, timestamp);
-      writer_->write(gps_msg, "kitti/oxts/gps/fix", timestamp);
+      write_message(gps_msg, "/kitti/oxts/gps/fix", timestamp);
 
       // write the velocity data to bag
       auto vel_msg = convert_oxts_to_vel_msg(oxts_parsed_array, timestamp);
       rclcpp::Time tmp1 = rclcpp::Time(timestamp.nanoseconds() + 10);
-      writer_->write(vel_msg, "kitti/oxts/gps/vel", tmp1);
+      write_message(vel_msg, "/kitti/oxts/gps/vel", tmp1);
 
       // write the IMU data to bag
       auto imu_msg = convert_oxts_to_imu_msg(oxts_parsed_array, timestamp);
       rclcpp::Time tmp2 = rclcpp::Time(timestamp.nanoseconds() + 20);
-      writer_->write(imu_msg, "kitti/oxts/imu", tmp2);
+      write_message(imu_msg, "/kitti/oxts/imu", tmp2);
     } else if (dir == "velodyne_points") {
       auto msg = convert_velo_to_msg(filename, timestamp);
-      writer_->write(msg, "kitti/velo", timestamp);
+      write_message(msg, "/kitti/velo", timestamp);
     }
   }
 
@@ -184,11 +229,11 @@ void Kitti2BagNode::get_all_timestamps()
         rclcpp::shutdown();
       }
 
-      // Extract microseconds and convert to duration
+      // Extract nanoseconds and convert to duration
       std::chrono::nanoseconds::rep nanoSecondsCount;
       iss >> nanoSecondsCount;
       if (iss.fail()) {
-        RCLCPP_ERROR(get_logger(), "Failed to parse microseconds.");
+        RCLCPP_ERROR(get_logger(), "Failed to parse nanoseconds.");
         rclcpp::shutdown();
       }
       auto nanoseconds = std::chrono::nanoseconds(nanoSecondsCount).count();
@@ -218,8 +263,7 @@ sensor_msgs::msg::Image Kitti2BagNode::convert_image_to_msg(
   return *msg;
 }
 
-std::vector<std::string> Kitti2BagNode::parse_file_data(
-  const fs::path & file_path, std::string delimiter)
+std::vector<std::string> Kitti2BagNode::parse_file_data(const fs::path & file_path)
 {
   std::ifstream input_file(file_path);
 
@@ -231,7 +275,7 @@ std::vector<std::string> Kitti2BagNode::parse_file_data(
   std::string file_content_string;
   if (input_file) {
     std::ostringstream ss;
-    ss << input_file.rdbuf(); // reading data
+    ss << input_file.rdbuf();
     file_content_string = ss.str();
   }
 
@@ -239,7 +283,7 @@ std::vector<std::string> Kitti2BagNode::parse_file_data(
   std::vector<std::string> tokens;
   size_t first = 0;
   while (first < file_content_string.size()) {
-    size_t second = file_content_string.find_first_of(delimiter, first);
+    size_t second = file_content_string.find_first_of(' ', first);
     //first has index of start of token
     //second has index of end of token + 1;
     if (second == std::string::npos) {
